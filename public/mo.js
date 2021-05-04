fc490ca4436c8e75400540c6b5fbc5cbe5ff3ddb
takhai <brahmanprince11@gmail.com>
2021-05-04 16:00:10 +0200

    whole site

---
+++ public/mo.js
@@ -0,0 +1,44 @@
+// alert('mi de ya')
+
+
+
+window.onload =  function() {
+
+ 
+test()
+  
+}//einde window onload
+
+
+
+
+// functies 
+
+function test(){
+
+
+  let sr = document.querySelector('section.jumbotron.text-center').addEventListener ()
+
+  //manipulate CSS
+
+  sr.style.visibility = "hidden";
+
+  
+
+  //  sr.style.margin = "10px", "20px","20px", "20px";
+
+      console.log(sr.style.visibility)
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
